91c994ca7f558bb10d89377ca472a473372386ce
ajpaulo <alferezjpaul@gmail.com>
2022-02-21 00:37:19 +0800

    Update settings.py

--- EURIQA/settings.py
+++ EURIQA/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'EURIQA.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'euriqa_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'NAME': 'euriqa-db',
+        'USER': 'euriqa@euriqa-db',
+        'PASSWORD': 'CITU_onlineadmissionsys',
+        'HOST': 'euriqa-db.mysql.database.azure.com',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
