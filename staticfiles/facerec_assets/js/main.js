e5a4ac3e060c102d8fd1bd1aa144adc0dcae8624
ajpaulo <alferezjpaul@gmail.com>
2022-03-06 03:18:28 +0800

    Update staticfiles

--- staticfiles/facerec_assets/js/main.js
+++ staticfiles/facerec_assets/js/main.js
@@ -79,7 +79,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(`/static2/profilephotos/${label}.jpg`)
+        const img = await faceapi.fetchImage(`https://euriqastorage.blob.core.windows.net/referencephotos/${label}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
